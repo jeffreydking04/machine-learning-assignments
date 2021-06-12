e340c54fbe57251198f8eb6a34f56b5302ba3406
Jeffrey King <jeffrey.king@trinityis.com>
2021-06-12 16:07:06 -0700

    math

---
+++ python_for_everybody/problems/probability/main.py
@@ -0,0 +1,11 @@
+import copy
+import random
+# Consider using the modules imported above.
+
+
+class Hat:
+    def __init__(*args):
+        print(*args)
+
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
