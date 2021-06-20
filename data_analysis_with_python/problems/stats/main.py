329251a80af55f643e581551bbfa8159297f69c8
Jeffrey King <jeffrey.king@trinityis.com>
2021-06-20 09:06:21 -0700

    refactor stats solution

--- data_analysis_with_python/problems/stats/main.py
+++ data_analysis_with_python/problems/stats/main.py
@@ -13,42 +13,28 @@ def calculate(list):
     Returns:
         Dictionary: keys: basic stats, values: array of axis 0 values, array of axis 1 values, flatten values
     """
+
     try:
-        np.array(list).reshape((3,3))
+        arr = np.array(list).reshape((3,3))
     except ValueError:
         raise ValueError('List must contain nine numbers.')
 
-    arr = np.array(list).reshape((3,3))
-    calculations = {
-        'mean': [
-            arr.mean(axis=0).tolist(),
-            arr.mean(axis=1).tolist(),
-            arr.mean()
-        ],
-        'variance': [
-            arr.var(axis=0).tolist(),
-            arr.var(axis=1).tolist(),
-            arr.var()
-        ],
-        'standard deviation': [
-            arr.std(axis=0).tolist(),
-            arr.std(axis=1).tolist(),
-            arr.std(),
-        ],
-        'max': [
-            arr.max(axis=0).tolist(),
-            arr.max(axis=1).tolist(),
-            arr.max(),
-        ],
-        'min': [
-            arr.min(axis=0).tolist(),
-            arr.min(axis=1).tolist(),
-            arr.min(),
-        ],
-        'sum': [
-            arr.sum(axis=0).tolist(),
-            arr.sum(axis=1).tolist(),
-            arr.sum(),
-        ],
+    stats_list = {
+        'mean': arr.mean,
+        'variance': arr.var,
+        'standard deviation': arr.std,
+        'max': arr.max,
+        'min': arr.min,
+        'sum': arr.sum,
     }
+
+    calculations = {}
+
+    for stat, func in stats_list.items():
+        calculations[stat] = [
+            func(axis=0).tolist(),
+            func(axis=1).tolist(),
+            func()
+        ]
+
     return calculations
